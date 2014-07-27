e2c8ef512808d12af35c43cdb6965e0aea96b5b8
F1-GTR <f1_kylikov@mail.ru>
2014-07-27 21:48:46 +0400

    Classes for "quest32.cpp" and test MTR file

---
+++ quest32.h
@@ -0,0 +1,31 @@
+#ifndef quest32H
+#define quest32H
+//---------------------------------------------------------------------------
+#include "tquest.h"
+
+class quest32: public tquest{
+  public:
+  char* strpar1;
+  char* strpar2;
+  int amin;
+  int amax;
+//  int dim;
+
+  quest32(FILE* f);
+  quest32(){};
+  Save(FILE* f);
+  copy(quest32* q){
+    strpar1 = strdup(q->strpar1);
+    strpar2 = strdup(q->strpar2);
+    amin = q->amin;
+    amax = q->amax;
+//    dim = q->dim;
+    tquest::copy(q);
+    return 0;
+  }
+  Edit();
+  Print(TList* plist);
+  Print(TList* plist, class test &t);
+};
+
+#endif
